722457beef11883b00114c6bf0b7b305548c2e7e
ganesh.pote@hotmail.com <ganesh.pote@hotmail.com>
2021-05-27 16:07:08 +0530

    Signed-off-by: ganesh.pote@hotmail.com <ganesh.pote@hotmail.com>

--- module/azure/vm/azurerm/main.tf
+++ module/azure/vm/azurerm/main.tf
@@ -253,7 +253,7 @@ resource "azurerm_availability_set" "vm" {
 
 resource "azurerm_public_ip" "vm" {
   count                        = "${var.nb_public_ip}"
-  name                         = "${var.vm_hostname}-${count.index}-publicIP"
+  name                         = "${var.vm_hostname}-${count.index}-publicip"
   location                     = local.location
   resource_group_name          = local.resource_group_name
   allocation_method 		   = "${var.public_ip_address_allocation}"
